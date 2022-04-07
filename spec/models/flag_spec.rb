3738030d0ff0d661ca7b622840b70387bc217a49
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-04-07 08:42:14 +0200

    Update flag model spec

--- spec/models/flag_spec.rb
+++ spec/models/flag_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Flag, type: :model do
   context 'validations:' do
+    # Length
+    it { should validate_length_of(:country_code).is_equal_to(3) } # ISO 3166-1 alpha-3 codes are three-letter country codes - https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+
+    # Number
+    it { should validate_numericality_of(:price) }
+
     # Presence
     it { should validate_presence_of(:country_code) }
     it { should validate_presence_of(:country) }
     it { should validate_presence_of(:currency_code) }
     it { should validate_presence_of(:image_url) }
     it { should validate_presence_of(:price) }
-
-    # Number
-    it { should validate_numericality_of(:price) }
   end
 end

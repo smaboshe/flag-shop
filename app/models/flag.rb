ea5a0c1169c3ecfd8334ee25e66ca857e5fa22ce
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-04-07 08:42:36 +0200

    Update the flag model

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,8 +1,15 @@
 class Flag < ApplicationRecord
+  # Length
+  validates :country_code, length: { is: 3 }
+
+  # Number
+  validates :price, numericality: true
+
+  # Presences
+  validates :country_code, presence: true
   validates :country_code, presence: true
   validates :country, presence: true
   validates :currency_code, presence: true
   validates :image_url, presence: true
   validates :price, presence: true
-  validates :price, numericality: true
 end

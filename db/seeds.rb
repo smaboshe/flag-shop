fa1b7e2925e3fd34eab5010324ce882cf67bc354
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-04-11 16:40:54 +0200

    Cleanup seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,3 @@ Flag.create( country: "South Africa",      country_code: "zaf", price: 750, curr
 Flag.create( country: "United Kingdom",    country_code: "gbr", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/uk-flag.gif")
 Flag.create( country: "United States",     country_code: "usa", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/us-flag.gif")
 Flag.create( country: "Zambia",            country_code: "zmb", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/za-flag.gif")
-
-# Same as above to test that duplicates don't get created
-Flag.create( country: "Australia",         country_code: "aus", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/as-flag.gif")
-Flag.create( country: "India",             country_code: "ind", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/in-flag.gif")
-Flag.create( country: "Nigeria",           country_code: "nga", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/ni-flag.gif")
-Flag.create( country: "Rwanda",            country_code: "rwa", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/rw-flag.gif")
-Flag.create( country: "South Africa",      country_code: "zaf", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/sf-flag.gif")
-Flag.create( country: "United Kingdom",    country_code: "gbr", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/uk-flag.gif")
-Flag.create( country: "United States",     country_code: "usa", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/us-flag.gif")
-Flag.create( country: "Zambia",            country_code: "zmb", price: 750, currency_code: "ZMW", image_url: "https://www.worldometers.info/img/flags/za-flag.gif")
